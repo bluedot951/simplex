bda350a550a6002fc3e44bc42af3ac9b559e1455
Shantanu Gore <sg937@cornell.edu>
2018-12-12 00:31:11 -0500

    automatic testing, parsing

--- parse.py
+++ parse.py
@@ -3,67 +3,52 @@ import simplex as s
 
 from scipy import optimize as opt
 
-f = open('test3.txt', 'r').read().split('\n')
-
-c = []
-AB = []
-
-m = 3
-n = 3
-
-stage = -1
-for line in f:
-	if line.strip() == 'C':
-		print 'stage 0'
-		stage = 0
-		continue
-	if line.strip() == 'A, B':
-		print 'stage 1'
-		stage = 1
-		continue
-
-	print line
-
-	if stage == 0:
-		c.extend([float(x) for x in line.split()])
-
-	else:
-		AB.extend([float(x) for x in line.split()])
+# f = open('test3.txt', 'r').read().split('\n')
 
 
-c = np.array(c)
-AB = np.array(AB).reshape((m, m+n+1))
 
-A = AB[:, :-1]
-b = AB[:, -1]
 
-print 'A', A
-print 'b', b
-print 'c', c
+# print 'A', A
+# print 'b', b
+# print 'c', c
 
-1/0
+# 1/0
 
 
 def test(filename, m, n):
-	f = open('test2.txt', 'r').read().split('\n')
+	f = open(filename, 'r').read().split('\n')
 
-	A = np.zeros((m, m+n))
-	b = np.zeros((m,1))
+	c = []
+	AB = []
 
-	for i in range(1, 2):
-		data = [float(x) for x in f[i].split()]
-		data = [d for d in data]
-		c = np.array([data])
+	stage = -1
+	for line in f:
+		if line.strip() == 'C':
+			# print 'stage 0'
+			stage = 0
+			continue
+		if line.strip() == 'A, B':
+			# print 'stage 1'
+			stage = 1
+			continue
 
-	count = 0
-	for i in range(3, m*2 + 3, 2):
-		# print i
-		# print f[i]
-		row = np.array([float(x) for x in f[i].split()])
-		bi = [float(x) for x in f[i+1].split()][0]
-		A[count, :] = row
-		b[count] = bi
-		count += 1
+		# print line
+
+		if stage == 0:
+			c.extend([float(x) for x in line.split()])
+
+		else:
+			AB.extend([float(x) for x in line.split()])
+
+
+	c = np.array(c)
+	AB = np.array(AB).reshape((m, m+n+1))
+
+	A = AB[:, :-1]
+	b = AB[:, -1]
+
+	b = np.expand_dims(b, axis=1)
+	c = np.expand_dims(c, axis=0)
 
 	b = b.T
 
@@ -90,8 +75,12 @@ def test(filename, m, n):
 
 	optV, vec = s.solve(A, b, c, 'max', False)
 
-	return np.allclose(optV, -1 * res['fun'])
+	return np.allclose(optV, -1 * res['fun']), optV
 
 print test('test.txt', 3, 2)
 print test('test2.txt', 3, 2)
-print 
+print test('test3.txt', 3, 3)
+print test('test4.txt', 10, 15)
+print test('test5.txt', 20, 35)
+print test('test6.txt', 200, 350)
+print test('test8.txt', 500, 500)
--- simplex.py
+++ simplex.py
@@ -23,13 +23,14 @@ def slack(A, c):
 	return A, c
 
 
-def solve(A, b, c, op):
+def solve(A, b, c, op, addSlack=True):
 
 	if op == 'min':
 		c = -1*c
 
 	_, n = A.shape
-	A, c = slack(A, c)
+	if addSlack:
+		A, c = slack(A, c)
 
 	# soln = np.zeros(c.shape)
 	soln = np.zeros(n)
@@ -37,50 +38,55 @@ def solve(A, b, c, op):
 	Ab = np.hstack((A,b.T))
 	Abc = np.vstack((Ab,c))
 
-	print 'Abc', Abc
+	# print 'Abc', Abc
 
 	idx = np.where(Abc[-1] > 0)[0]
-	print idx
+	# print idx
+	num_pivots = 0
+
 	while len(idx) > 0:
-		print '================='
-		i = idx[0]
-		print i
+		num_pivots += 1
+		# print '================='
+		i = np.random.choice(idx)
+		# print i
 		coeffs = np.divide(Abc[:,-1], Abc[:,i], out=np.full(Abc.shape[0], np.inf), where=Abc[:,i]!=0)
 		coeffs[coeffs < 0] = np.inf
 
 		assert len(np.where(coeffs != np.inf)[0]) > 0
 
 		# coeffs = Abc[:,-1]/Abc[:,i]
-		print Abc[:,-1], Abc[:,i], coeffs[:-1]
+		# print Abc[:,-1], Abc[:,i], coeffs[:-1]
 		j = np.argmin(coeffs[:-1])
-		print j
+		# print j
 		Abc[j] = 1/Abc[j,i]*Abc[j]
 
 		oldAbc = np.copy(Abc)
 		for k in range(Abc.shape[0]):
 			if k==j: continue
 			Abc[k] = oldAbc[k] - oldAbc[k,i]*oldAbc[j]
-		print Abc
+		# print Abc
 		idx = np.where(Abc[-1] > 0)[0]
 
 	UL = Abc[:-1, :-1]
 	for i in range(UL.shape[1] - UL.shape[0]):
 		nz = np.nonzero(UL[:, i])[0]
-		print nz
+		# print nz
 		if len(nz) == 1:
-			print Abc[nz[0], -1], Abc[nz[0], i]
+			# print Abc[nz[0], -1], Abc[nz[0], i]
 			soln[i] = Abc[nz[0], -1] / Abc[nz[0], i]
-		print '---'
+		# print '---'
 
 	optVal = Abc[-1,-1]
 
 	if op == 'max':
 		optVal = -1*optVal 
 	
+	print 'pivots', num_pivots
+
 	return optVal, soln
 
-out = solve(A,b,c,'max')
-print 'sol: ', out
+# out = solve(A,b,c,'max')
+# print 'sol: ', out
 
 # i = 1
 # j = 2
